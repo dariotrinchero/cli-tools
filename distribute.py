3f2450e1aa0af46fa4656d158d9e5ca3217a788f
Dario Trinchero <dario.trinchero@gmail.com>
2019-05-20 01:01:03 +0200

    Added progress bars and more intelligent functionality and output

--- distribute.py
+++ distribute.py
@@ -11,6 +11,8 @@ from itertools import count
 # Usage Notes & Examples (run with -h for full help):
 #---------------------------------------------------------------------------------------------------
 #
+# In this script, "parent" refers to the containing directory of this file.
+#
 # Distribute & compare contents to ~/Music
 #  $ distribute.py
 #
@@ -19,43 +21,124 @@ from itertools import count
 #
 
 
-def get_path(i, f = ''):
-    ''' Returns path of ix255, parent if i==0 or ~/Music if i<0. If f is non-empty, it specifies a
-    file within this folder. '''
+def get_path(i, f = None):
+    ''' Returns path of subdirectory ix255, or containing directory if i is 0, or ~/Music if i < 0;
+    if f is given, it specifies a particular file in this directory '''
     gpath = os.path.dirname(os.path.realpath(sys.argv[0]))
+
     if i == 0: path = gpath
     elif i < 0: path = os.environ['HOME'] + '/Music'
     else: path = gpath + '/' + str(i) + 'x255'
-    if f == '': return path
+
+    if f == None: return path
     return path + '/' + f
 
 def files(i):
-    ''' Returns files in get_path(i). '''
+    ''' Returns list of name of all files in subdirectory ix255 '''
     return sorted(os.listdir(get_path(i)), key=str.lower)
 
+def file_lists():
+    ''' Generator to iterate through all subdirectories, yielding number of subdirectory and list
+    of contained files for each '''
+    parent_files = files(0)
+    for i in count(1):
+        if str(i) + 'x255' in parent_files: yield i, files(i)
+        else: break
+
 def move(f, i, j):
-    ''' Moves file with name f from get_path(i) to get_path(j). '''
+    ''' Moves file with name f from subdirectory ix255 to jx255 '''
     global moved
     try: os.mkdir(get_path(j))
     except: pass
     shutil.move(get_path(i, f), get_path(j, f))
     moved[f] = 'parent' if j == 0 else str(j) + 'x255'
 
-def check_synched(f, i, compare = True):
-    ''' Check that file given by get_path(i, f) corresponds to one in ~/Music '''
-    if not os.path.isfile(get_path(-1, f)):
-        print('No file named "' + f + '" exists in ~/Music')
+def redistribute():
+    ''' Redistribute files so that each subdirectory contains at most 255 files, filling from
+    lower-numbered directories upwards '''
+    global moved
+    parent_files = files(0)
+    print('Redistributing files ...')
+
+    for i, ifiles in file_lists():
+        n = len(ifiles)
+        if n > 255:
+            for f in ifiles[255:]: move(f, i, i + 1)
+        elif n < 255 and str(i + 1) + 'x255' in parent_files:
+            for f in files(i + 1)[:255 - n]: move(f, i + 1, i)
+
+def heading(text):
+    ''' Display given text as a heading with width scaling with terminal '''
+    width = shutil.get_terminal_size((80, 20))[0]
+    print(('{:_^' + str(width) + '}\n').format('  ' + text + '  '))
+
+def new_prog_bar(i):
+    ''' Initialize a new (dynamically sized) progress bar '''
+    global prog, width
+    prog = 0
+    width = shutil.get_terminal_size((80, 20))[0] - 8 - len(str(i))
+    sys.stdout.write(str(i) + 'x255: [' + '.' * width + ']')
+    sys.stdout.flush()
+    sys.stdout.write('\b' * width)
+
+def update_prog_bar(t):
+    ''' Update the current progress bar to reflect progress t (0 < t <= 1) '''
+    global prog, width
+    if t == 1: sys.stdout.write('#' * (width - prog) + ']\n')
+    else:
+        d = int(t * width - prog)
+        if d > 0:
+            sys.stdout.write('#' * d)
+            sys.stdout.flush()
+            prog += d
+
+def check_synched(compare = True):
+    ''' Check that all files in all subdirectories correspond to a matching file in ~/Music '''
+    missing, different = [], []
+
+    if compare: heading('Checking files against ~/Music:')
+    else: print('Checking files against ~/Music ...')
+
+    # Record files which are missing or differ to ~/Music
+    for i, ifiles in file_lists():
+        n = len(ifiles)
+        if compare: new_prog_bar(i)
+
+        for j in range(n):
+            f = ifiles[j]
+            if compare: update_prog_bar(float(j) / n)
+
+            if not os.path.isfile(get_path(-1, f)): missing.append((i, f))
+            elif compare and not filecmp.cmp(get_path(-1, f), get_path(i, f)):
+                different.append((i, f))
+
+        if compare: update_prog_bar(1)
+
+    # Report missing / differing files
+    deleted = False
+    if len(missing) != 0 or len(different) != 0:
+        if compare: print()
+        heading('Missing and differing files:')
+
+    for (i, f) in missing:
+        print(str(i) + 'x255: No file named "' + f + '" exists in ~/Music')
         if input('Remove file? [y/n]: ') in 'yY':
             os.remove(get_path(i, f))
+            deleted = True
             print('File removed.')
-    elif compare and not filecmp.cmp(get_path(-1, f), get_path(i, f)):
-        print('File "' + f + '" differs from that in ~/Music')
+
+    for (i, f) in different:
+        print(str(i) + 'x255: File "' + f + '" differs from that in ~/Music')
         if input('Update file? [y/n]: ') in 'yY':
             shutil.copy(get_path(-1, f), get_path(i, f))
             print('File updated.')
 
+    # Redistribute if any files were deleted
+    if deleted:
+        print()
+        redistribute()
+
 moved = {} # Stores final destinations of files moved
-parent_files = files(0)
 
 if __name__ == '__main__':
     # Create argument parser and parse args
@@ -72,22 +155,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Move all .mp3 files from parent into 1x255
-    for f in parent_files:
+    for f in files(0):
         if f[-4:] == '.mp3': move(f, 0, 1)
 
     # Redistribute files & check against ~/Music
-    for i in count(1):
-        if str(i) + 'x255' in parent_files:
-            ifiles = files(i)
-            for f in ifiles: check_synched(f, i, args.compare)
+    redistribute()
+    check_synched(args.compare)
 
-            n = len(ifiles)
-            if n > 255:
-                for f in ifiles[255:]: move(f, i, i + 1)
-            elif n < 255 and str(i + 1) + 'x255' in parent_files:
-                for f in files(i + 1)[:255 - n]: move(f, i + 1, i)
-        else: break
-
-    # Report changes (a single file moved many times is reported once)
+    # Report moved files (only final destination reported)
+    if len(moved) != 0: heading('Moved files:')
     for f in moved: print('Moved "' + f + '" into ' + moved[f])
 
