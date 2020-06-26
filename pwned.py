ca870015a34b346a46900b76d9fc152c7d34be19
Dario Trinchero <dario.trinchero@gmail.com>
2020-06-26 21:58:10 +0200

    Added code for new optional response padding for increased privacy

--- pwned.py
+++ pwned.py
@@ -27,34 +27,33 @@ def get_password(reveal):
 def get_hashes(password):
     ''' Hash given password and retrieve list of hashes matching prefix of hash '''
     pw_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    try:
-        hash_list = requests.get(url='https://api.pwnedpasswords.com/range/{}'.format(pw_hash[:5]))
-    except:
-        print('ERROR: Cannot connect to API.')
-        hash_list = None
+    try: hash_list = requests.get(url='https://api.pwnedpasswords.com/range/{}'.format(pw_hash[:5]),
+                headers={'Add-Padding': 'true'})
+    except: hash_list = None
     return pw_hash, hash_list
 
 def find_matches(hash_list, pw_hash, pw_str):
     ''' Check given list of hashes for exact matches with given hash, and report matches '''
     for match in hash_list.text.split('\r\n'):
         split = match.split(':')
-        if pw_hash[5:] == split[0]:
-            occurrences = '{:,d} occurrence{}'.format(int(split[1]), '' if split[1] == '1' else 's')
-            print('%s was found\nHash %s\t%s' % (pw_str, pw_hash, occurrences))
+        occurences = int(split[1])
+        if occurences > 0 and pw_hash[5:] == split[0]:
+            print('{} was found\nHash {}\t{:,d} occurence{}'.format(pw_str, pw_hash, occurences,
+                (occurences > 1) * 's'))
             break
     else: print('%s was not found' % pw_str)
 
 
 if __name__ == '__main__':
     # Create argument parser and parse args
-    parser = argparse.ArgumentParser(description='Reads in passwords and checks whether matching \
-            passwords have been leaked in data breaches. For this it queries the "Have I Been \
+    parser = argparse.ArgumentParser(description='Reads in passwords and checks whether matching\
+            passwords have been leaked in data breaches. For this it queries the "Have I Been\
             Pwned" API detailed at https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange\
             and searches through the results for matching passwords.')
     parser.add_argument('--reveal', action='store_true',
             help='show entered password in plaintext (default behaviour hides entry)')
     parser.add_argument('--loop', action='store_true',
-            help='keep prompting for passwords until given EOF or empty input (default checks \
+            help='keep prompting for passwords until given EOF or empty input (default checks\
                     single password)')
     args = parser.parse_args()
 
@@ -63,7 +62,7 @@ if __name__ == '__main__':
         if password == '': break
 
         pw_hash, hash_list = get_hashes(password)
-        if hash_list == None: break
+        if hash_list == None: exit('ERROR: Cannot connect to API')
 
         find_matches(hash_list, pw_hash, pw_str)
         if not args.loop: break
