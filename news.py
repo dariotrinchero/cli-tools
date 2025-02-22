fc78e9840f05921e322f59e813b1afd4c52a384d
Dario Trinchero <dario.trinchero@pm.me>
2025-02-22 11:49:25 +0200

    Make script more idiomatic by introducing classes.
    Make category headings colored.
    Catch exceptions caused by user exiting early.

--- news.py
+++ news.py
@@ -6,7 +6,6 @@ import sys
 import textwrap
 import subprocess
 
-from math import nan, isnan
 from datetime import date, timedelta
 from shutil import get_terminal_size
 
@@ -29,40 +28,10 @@ from urllib.error import HTTPError, URLError
 #
 #---------------------------------------------------------------------------------------------------
 
-def human_date(day):
-    ''' Convert date to string in verbose human-readable format. '''
-    return day.strftime('%A, %-d %B %Y')
-
-def get_news(day):
-    ''' Retrieve current events for given day from Wikipedia; return list of lines. '''
-    day_str = day.strftime('%Y_%B_%d')
-    try:
-        req = urlopen(f'https://en.wikipedia.org/wiki/Portal:Current_events/{day_str}?action=raw')
-    except (HTTPError, URLError) as err:
-        exit(f'Unable to retrieve news for {human_date(day)}: {err}')
-    return bytes(req.read()).decode('utf-8').split('\n')[3:-1] # head & tail aren't content
-
-def valid_url(url):
-    ''' Return whether given string is a well-formed URL. '''
-    try:
-        parsed = urlparse(url)
-        return all([parsed.scheme, parsed.netloc])
-    except AttributeError:
-        return False
-
-def parse_link(dest, text=None):
-    ''' Get hyperlink URL & display test for given destination, possibly a wikilink. '''
-    if not valid_url(dest): # convert internal wikilink to URL
-        if text is None: text = dest
-        target = quote(dest.replace(' ', '_'))
-        dest = f'https://en.wikipedia.org/wiki/{target}'
-    return (dest, dest if text is None else text)
-
 class Term:
     ''' Contains functions to apply ANSI terminal formatting to text. '''
-    ANSI_SUPPORTED = True
 
-    # ANSI escape sequences
+    # ANSI styles (& their escape sequences)
     BOLD  = ('[1m', '[22m')
     DIM   = ('[2m', '[22m')
     ITAL  = ('[3m', '[23m')
@@ -70,71 +39,174 @@ class Term:
     COLOR = ('[38;2;%d;%d;%dm', '[39m')
     LINK  = (']8;;', '\\')
 
-    def fmt(text, *styles):
-        if not Term.ANSI_SUPPORTED: return text
+    def __init__(self, width, indent, compact=False, ansi_override=None):
+        if ansi_override is not None: self.ansi = ansi_override
+        else: self.ansi = sys.stdout.isatty()
+        self.width = width
+        self.indent = indent
+        self.compact = compact
+
+    def fmt(self, text, *styles):
+        if not self.ansi: return text
         for style in styles:
             text = '\033' + style[0] + text + '\033' + style[1]
         return text
 
-    def link(url, text):
-        if not Term.ANSI_SUPPORTED: return text
-        return Term.fmt(url, Term.LINK) + text + Term.fmt('', Term.LINK)
+    # shorthand aliases
+    def bold(self, text): return self.fmt(text, Term.BOLD)
+    def dim(self, text): return self.fmt(text, Term.DIM)
+    def ital(self, text): return self.fmt(text, Term.ITAL)
+    def under(self, text): return self.fmt(text, Term.UNDER)
+
+    def color(self, text, color):
+        return self.fmt(text, (Term.COLOR[0] % color, Term.COLOR[1]))
+
+    def link(self, url, text):
+        if not self.ansi: return text
+        return self.fmt(url, Term.LINK) + text + self.fmt('', Term.LINK)
     
-    def heading(text, color=(255, 255, 90)):
-        if not Term.ANSI_SUPPORTED: return text
-        return Term.fmt(text, Term.UNDER, Term.BOLD, Term.COLOR) % color
+    def heading(self, text, color=(255, 255, 90)):
+        return self.color(self.fmt(text, Term.UNDER, Term.BOLD), color)
 
-def bullet_lvl(lines, line):
-    ''' Get indent level of bullet on given line (NaN if line out of range, or not bullet list item). '''
-    if not 0 < line < len(lines): return nan
-    bullets = re.search('^\*+(?=[^*])', lines[line])
-    return nan if not bullets else bullets.end()
-    
-def print_news(day, width=100, indent=3, compact=False):
-    ''' Prints news headlines for given day from Wikipedia, formatted for terminal output. '''
-    # print date heading & loading filler text
-    print(Term.heading(human_date(day)) + '\n\n'
-        + Term.fmt('fetching...\r', Term.DIM, Term.ITAL) * Term.ANSI_SUPPORTED, end='')
-    sys.stdout.flush()
-
-    # fetch headlines
-    news = get_news(day)
-    for l, line in enumerate(news):
-        # conceal link URLs for better line wrapping
-        link_urls = []
-        def hide_link(m, delim):
-            url, text = parse_link(m.group(1), m.group(3))
-            link_urls.insert(0, url)
-            return delim + text.replace(' ', '\u00A0') + delim # non-breaking space
-
-        line = re.sub(r'\[\[(.+?)(\|(.+?))?\]\]', lambda m: hide_link(m, '\u00b6'), line)
-        line = re.sub(r'\[([^ ]+)( (\(.+?\)))?\]', lambda m: hide_link(m, '\u204b'), line)
-
-        # bullet points & line wrapping
+    def wrap(self, line, list_lvl=0):
         indent_args = {}
-        lvl = bullet_lvl(news, l)
-        if not isnan(lvl):
-            line = line[lvl:].strip()
-            dense = compact or bullet_lvl(news, l - 1) < lvl < bullet_lvl(news, l + 1)
-            indent_args['initial_indent'] = '\n' * (not dense) + ' ' * indent * (lvl - 1) + '\u2022 '
-            indent_args['subsequent_indent'] = ' ' * (indent * (lvl - 1) + 2)
-        line = textwrap.fill(line, width, break_long_words=False, **indent_args)
+        if list_lvl > 0:
+            indent_args['initial_indent'] = ' ' * self.indent * (list_lvl - 1) + '\u2022 '
+            indent_args['subsequent_indent'] = ' ' * (self.indent * (list_lvl - 1) + 2)
+        return textwrap.fill(line, self.width, break_long_words=False, **indent_args)
 
-        # bold & italics
-        line = re.sub("'''(.+?)'''", lambda m: Term.fmt(m.group(1), Term.BOLD), line)
-        line = re.sub("''(.+?)''", lambda m: Term.fmt(m.group(1), Term.ITAL), line)
+    def line_space(self, line):
+        return line if self.compact else '\n' + line
 
-        # restore link destinations
-        show_link = lambda m: Term.link(link_urls.pop(), m.group(1))
-        line = re.sub('\u00b6(.+?)\u00b6', show_link, line)
-        line = re.sub('\u204b(.+?)\u204b', show_link, line)
 
-        print(line)
+class WikiNews:
+    ''' Class for parsing Wikipedia news, as per specification (https://w.wiki/DA2e). '''
+
+    CATEGORIES = { # headline categories
+        # this list is exhaustive; cf. https://w.wiki/DA2e
+        'Arts and culture':            '🎨',
+        'Armed conflicts and attacks': '⚔️ ',
+        'Business and economy':        '📈',
+        'Disasters and accidents':     '🌊',
+        'Health and environment':      '🌱',
+        'International relations':     '🌐',
+        'Law and crime':               '⚖️ ',
+        'Politics and elections':      '🗳 ',
+        'Science and technology':      '🧬',
+        'Sports':                      '🏀'
+    }
+
+    DELIM = '\x1F' # unit separator; delimits links with hidden URLs
+
+    RE = { # patterns for parsing news
+        'bold':    re.compile("'''(.+?)'''"),
+        'bullet':  re.compile('^\*+(?=[^*])'),
+        'heading': re.compile("^'''(.+)'''$"),
+        'ital':    re.compile("''(.+?)''"),
+        'link':    re.compile(r'(\[\[(.+?)(\|(.+?))?\]\])|(\[([^ []+)( (\(.+?\)))?\])'),
+        'linktxt': re.compile(f'{DELIM}(.+?){DELIM}')
+    }
+
+    def __init__(self, day, heading_icons=True):
+        self.heading_icons = heading_icons
+
+        # retrieve news for given day from Wikipedia as list of lines
+        day_str = day.strftime('%Y_%B_%d')
+        try:
+            req = urlopen(f'https://en.wikipedia.org/wiki/Portal:Current_events/{day_str}?action=raw')
+        except (HTTPError, URLError) as err:
+            exit(f'Unable to retrieve news for {day.strftime("%-d %B %Y")}: {err}')
+        self.news = bytes(req.read()).decode('utf-8').split('\n')[3:-1] # trim non-content
+
+    def format(self, formatter):
+        ''' Yield news lines, formatted using given formatter. '''
+        for l, line in enumerate(self.news):
+            # conceal link URLs for better line wrapping
+            urls = []
+            line = WikiNews.RE['link'].sub(lambda m: WikiNews.__hide_url(m, urls), line)
+
+            # bold, italics & headings
+            line = self.__format_heading(line, formatter)
+            line = WikiNews.RE['bold'].sub(lambda m: formatter.bold(m.group(1)), line)
+            line = WikiNews.RE['ital'].sub(lambda m: formatter.ital(m.group(1)), line)
+
+            # bullet points & line wrapping
+            lvl = self.__list_lvl(l)
+            line = formatter.wrap(line[lvl:].strip(' '), lvl)
+
+            # add spacing if level is not between levels of adjacent lines
+            if lvl > 0 and not (0 < self.__list_lvl(l - 1) < lvl < self.__list_lvl(l + 1)):
+                line = formatter.line_space(line)
+
+            # restore link URLs
+            line = WikiNews.RE['linktxt'].sub(lambda m: formatter.link(urls.pop(), m.group(1)), line)
+
+            yield line
+
+    def is_blank(self):
+        ''' Return whether there is no news for given day. '''
+        return self.news == ['*'] # default empty template
+
+    def __list_lvl(self, line):
+        ''' Get indent level of bullet on given line (0 if out of range, or not list item). '''
+        if not 0 < line < len(self.news): return 0
+        bullets = WikiNews.RE['bullet'].search(self.news[line])
+        return 0 if not bullets else bullets.end()
+
+    def __format_heading(self, line, formatter, color=(255, 255, 170)):
+        ''' Apply appropriate formatting to line if heading; else leave unchanged. '''
+        m = WikiNews.RE['heading'].match(line)
+        if m is None: return line
+        heading = m.group(1).capitalize()
+        if heading not in WikiNews.CATEGORIES: return line
+
+        icon = WikiNews.CATEGORIES[heading] + ' ' if self.heading_icons else ''
+        return icon + formatter.bold(formatter.color(heading, color))
+
+    @staticmethod
+    def __hide_url(m, urls):
+        ''' Take regex match for link, & list of URLs; append URL to list, & return link text. '''
+        url, text = (m.group(2), m.group(4)) if m.group(2) else (m.group(6), m.group(8))
+
+        # get URL for internal wikilink
+        if not WikiNews.__valid_url(url):
+            if text is None: text = url
+            target = quote(url.replace(' ', '_'))
+            url = f'https://en.wikipedia.org/wiki/{target}'
+
+        # wrap display text in delimiters & make spaces non-breaking
+        if text is None: text = url
+        text = WikiNews.DELIM + text.replace(' ', '\u00A0') + WikiNews.DELIM
+
+        # record hidden URL in list & return display text
+        urls.insert(0, url)
+        return text
+
+    @staticmethod
+    def __valid_url(url):
+        ''' Return whether given string is well-formed URL. '''
+        try:
+            parsed = urlparse(url)
+            return all([parsed.scheme, parsed.netloc])
+        except AttributeError: return False
+
+
+def print_news(days, term, heading_icons=True):
+    ''' Print news headlines from Wikipedia for given number of days. '''
+    today = date.today()
+    for d in range(days):
+        day = today - timedelta(days=d)
+
+        if d > 0: print() # separating line
+        print(term.heading(day.strftime('%A, %-d %B %Y')) + '\n')
+        print(term.fmt('fetching...\r', Term.DIM, Term.ITAL) * term.ansi, end='')
+        sys.stdout.flush()
+
+        news = WikiNews(day, heading_icons)
+        print(term.ital("(no news)  ") if news.is_blank() else '\n'.join(news.format(term)))
+
 
 if __name__ == '__main__':
-    # base default width on terminal size
-    width = min(int(0.8 * get_terminal_size((120, 24)).columns), 120)
-
     # create argument parser & parse args
     parser = argparse.ArgumentParser(description="Retrieves & displays recent news headlines, as\
         retrieved from the Wikipedia 'Portal:Current events' page. Headlines are rendered as bullet\
@@ -142,29 +214,33 @@ if __name__ == '__main__':
         sources.")
     parser.add_argument('days', nargs='?', type=int, default=None, help='number of days (including\
         today) to retrieve (defaults to 2, or 7 if --less is given)')
-    parser.add_argument('--width', type=int, default=width, help='maximum output width')
+    parser.add_argument('--width', type=int, default=None, help='maximum output width')
     parser.add_argument('--indent', type=int, default=3, help='width of indent for nested lists')
     parser.add_argument('--compact', action='store_true', help='reduce blank lines between bullets')
     parser.add_argument('--ansi', choices=['y', 'n', 'auto'], default='auto',
         help='whether to apply ANSI styling to output')
     parser.add_argument('--less', action='store_true', help='automatically pipe output through less\
         to get scrolling newsfeed')
+    parser.add_argument('--no-icons', action='store_false', dest='icons', help='suppress emoji in\
+        headline category headings')
     args = parser.parse_args()
 
-    # check whether to apply ansi styling
-    Term.ANSI_SUPPORTED = args.ansi == 'y' or args.ansi == 'auto' and sys.stdout.isatty()
+    # create terminal formatter
+    width = args.width or min(int(0.8 * get_terminal_size((120, 24)).columns), 120)
+    term = Term(width, args.indent, args.compact, None if args.ansi == 'auto' else args.ansi == 'y')
 
-    if args.less:
-        # pipe stdout to stdin of 'less' subprocess
-        less_proc = subprocess.Popen(['less', '-c', '-r'], stdin=subprocess.PIPE, text=True)
-        sys.stdout = less_proc.stdin
+    try:
+        if args.less:
+            # attach stdout to stdin of 'less' subprocess
+            less_proc = subprocess.Popen(['less', '-crK'], stdin=subprocess.PIPE, text=True)
+            sys.stdout = less_proc.stdin
 
-    # output news for each day
-    today = date.today()
-    for d in range(args.days or 7 if args.less else 2):
-        if d > 0: print()
-        print_news(today - timedelta(days=d), args.width, args.indent, args.compact)
+        # print news headlines
+        print_news(args.days or (5 if args.less else 2), term, args.icons)
 
-    if args.less:
-        less_proc.stdin.close()
-        less_proc.wait()
+    except (BrokenPipeError, KeyboardInterrupt): pass # ignore user exiting early
+
+    finally:
+        if args.less:
+            less_proc.stdin.close()
+            less_proc.wait()
